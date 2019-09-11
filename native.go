192d74ca1a396b61d772df0d17d90debf90966f7
panchengtao <pan.chengtao@synyi.com>
2019-09-11 14:45:08 +0800

    refactor: remove unused(may be) field

--- native.go
+++ native.go
@@ -20,7 +20,6 @@ type object_data struct {
 	typ    reflect.Type
 	val    reflect.Value
 	method int
-	uid    *int64
 }
 
 var (
@@ -245,7 +244,6 @@ func setNativeFieldFromJSValue(field reflect.Value, ctx *Context, value *Value) 
 
 func register(data *object_data) int64 {
 	id := atomic.AddInt64(&uid, 1)
-	data.uid = &id
 	objects.Store(id, data)
 	return id
 }
@@ -268,7 +266,7 @@ func (ctx *Context) NewFunctionWithCallback(callback GoFunctionCallback) *Object
 	data := &object_data{
 		reflect.TypeOf(callback),
 		reflect.ValueOf(callback),
-		0, nil}
+		0}
 	id := register(data)
 
 	ret := C.JSObjectMake(ctx.ref, nativecallback, unsafe.Pointer(&id))
@@ -314,7 +312,7 @@ func (ctx *Context) NewFunctionWithNative(fn interface{}) *Object {
 	data := &object_data{
 		reflect.TypeOf(fn),
 		reflect.ValueOf(fn),
-		0, nil}
+		0}
 	id := register(data)
 
 	ret := C.JSObjectMake(ctx.ref, nativefunction, unsafe.Pointer(&id))
@@ -390,7 +388,7 @@ func (ctx *Context) NewNativeObject(obj interface{}) *Object {
 	data := &object_data{
 		reflect.TypeOf(obj),
 		reflect.ValueOf(obj),
-		0, nil}
+		0}
 	id := register(data)
 
 	ret := C.JSObjectMake(ctx.ref, nativeobject, unsafe.Pointer(&id))
@@ -503,7 +501,7 @@ func newNativeMethod(ctx *Context, obj *object_data, method int) *Object {
 	data := &object_data{
 		obj.typ,
 		obj.val,
-		method, nil}
+		method}
 	id := register(data)
 
 	ret := C.JSObjectMake(ctx.ref, nativemethod, unsafe.Pointer(&id))
